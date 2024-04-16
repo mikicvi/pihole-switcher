dcb1b9fcfc50f22ea7548827991742a0039d126b
mikicvi <mika5566@gmail.com>
2024-04-16 23:20:30 +0100

    format entire project, implement fluentui tabs

--- README.md
+++ README.md
@@ -1,21 +1,24 @@
 # Pihole switcher
-This project was built to serve as frontend for local Pihole instance. 
 
-It's goal was to simplify interaction with pihole, and enable user to switch off/on pihole ad blocking service on the network for "X" amount of time, and display top ads and top requests. 
+This project was built to serve as frontend for local Pihole instance.
+
+It's goal was to simplify interaction with pihole, and enable user to switch off/on pihole ad blocking service on the network for "X" amount of time, and display top ads and top requests.
 
 Primary design of the app was mobile oriented, but it looks pretty decent on desktop too.
 
 ## Features
-- Enable or disable ad blocking service
-- Display current status of the service
-- Easy access to pihole control dashboard
-- Interactive Pie chart displaying top ads or top queries
-- Light/Dark mode
 
+-   Enable or disable ad blocking service
+-   Display current status of the service
+-   Easy access to pihole control dashboard
+-   Interactive Pie chart displaying top ads or top queries
+-   Light/Dark mode
 
 ## Setting up
+
 This project was built to be served from docker.
-- To build a docker image execute from root of project:
+
+-   To build a docker image execute from root of project:
 
 `yarn install && yarn build`
 
@@ -32,7 +35,7 @@ docker run -d -p 3016:80 \
   pihole-switcher-prod:latest
 ```
 
-- Parameters explanation
+-   Parameters explanation
 
     -d: Run the container in detached mode (in the background).
 
@@ -43,9 +46,10 @@ docker run -d -p 3016:80 \
     -e REACT_APP_PIHOLE_BASE=< e.g http://192.168.1.1:8080/admin/>: Set the REACT_APP_PIHOLE_BASE environment variable with the specified base URL.
 
     mikicv/pihole-switcher:latest : Specify the image name and tag.
+
 ---
 
-If you preffer docker compose instead 
+If you preffer docker compose instead
 
 ```docker compose
 version: '3'
@@ -60,28 +64,30 @@ services:
       - REACT_APP_PIHOLE_BASE=<pihole base URL> e.g http://192.168.1.1:8080/admin/
 ```
 
-***Make sure you replace pihole API key and your pihole base URL with your actual Pihole API key and ensure that the pihole-switcher-prod:latest image is available on your system.*** 
-
+**_Make sure you replace pihole API key and your pihole base URL with your actual Pihole API key and ensure that the pihole-switcher-prod:latest image is available on your system._**
 
 # High level overview
+
 This app interacts with pihole HTTP API
 
-More info about it can be found in [this](https://discourse.pi-hole.net/t/pi-hole-api/1863) forum post. 
+More info about it can be found in [this](https://discourse.pi-hole.net/t/pi-hole-api/1863) forum post.
 
 ## Development:
-- For development purposes, .env.local file has to be created with ENV params mentioned above in root.
-- This will require you to have a pihole instance running either bare-metal or in docker.
-- Latest version of: Yarn and Node
 
+-   For development purposes, .env.local file has to be created with ENV params mentioned above in root.
+-   This will require you to have a pihole instance running either bare-metal or in docker.
+-   Latest version of: Yarn and Node
 
 # Preview
+
 ![Pihole-switcher-preview](https://github.com/mikicvi/pihole-switcher/assets/88291034/35bf1884-8e6d-4c6d-b7b7-089296017785)
 ![Pihole-switcher-preview-2](https://github.com/mikicvi/pihole-switcher/assets/88291034/25f47202-5e55-403d-9dcd-f9805efb7c40)
 
 ## Notice:
-- This is a work in progress. Any suggestions are more than welcome, as well as feature suggestions and PR's.
-- This project is built on Typescript/NodeJS/React, so any contributions should be within this stack.
 
+-   This is a work in progress. Any suggestions are more than welcome, as well as feature suggestions and PR's.
+-   This project is built on Typescript/NodeJS/React, so any contributions should be within this stack.
 
 ---
+
 Contact: <mika5566@gmail.com>
--- src/App.tsx
+++ src/App.tsx
@@ -1,191 +1,206 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState } from 'react';
 import {
-    Toggle,
-    Dropdown,
-    IDropdownOption,
-    initializeIcons,
-    ThemeProvider,
-} from "@fluentui/react";
-import axios from "axios";
-import "./App.css";
-import piholeLogo from "./images/pihole.png";
-import { darkTheme, lightTheme } from "./themes/themes";
-import Graph from "./Graph";
+	Toggle,
+	Dropdown,
+	IDropdownOption,
+	initializeIcons,
+	ThemeProvider,
+} from '@fluentui/react';
+import axios from 'axios';
+import './App.css';
+import piholeLogo from './images/pihole.png';
+import { darkTheme, lightTheme } from './themes/themes';
+import Graph from './Graph';
 
 initializeIcons(); // Initialize Fluent UI icons
 
 const PiholeSwitcher: React.FC = () => {
-    const [ selectedTime, setSelectedTime ] = useState<string | undefined>(
-        undefined
-    );
-    const [ toggleOn, setToggleOn ] = useState<boolean>(false);
-    const [ isDarkMode, setIsDarkMode ] = useState<boolean>(true);
-    const [ timeLeft, setTimeLeft ] = useState<any>(null);
-    const [ piholeStatus, setPiholeStatus ] = useState<string>("false");
-    let [ timer, setTimer ] = useState<any>(null);
-    const [ isLogoHovered, setIsLogoHovered ] = useState<boolean>(false);
-    const [ tooltipPosition, setTooltipPosition ] = useState<any>({ top: 0, left: 0 });
+	const [selectedTime, setSelectedTime] = useState<string | undefined>(
+		undefined
+	);
+	const [toggleOn, setToggleOn] = useState<boolean>(false);
+	const [isDarkMode, setIsDarkMode] = useState<boolean>(true);
+	const [timeLeft, setTimeLeft] = useState<any>(null);
+	const [piholeStatus, setPiholeStatus] = useState<string>('false');
+	let [timer, setTimer] = useState<any>(null);
+	const [isLogoHovered, setIsLogoHovered] = useState<boolean>(false);
+	const [tooltipPosition, setTooltipPosition] = useState<any>({
+		top: 0,
+		left: 0,
+	});
 
-    const timeOptions: IDropdownOption[] = [
-        { key: "300", text: "5 minutes" },
-        { key: "900", text: "15 minutes" },
-        { key: "3600", text: "60 minutes" },
-        { key: "86400", text: "24 hours" },
-    ];
+	const timeOptions: IDropdownOption[] = [
+		{ key: '300', text: '5 minutes' },
+		{ key: '900', text: '15 minutes' },
+		{ key: '3600', text: '60 minutes' },
+		{ key: '86400', text: '24 hours' },
+	];
 
-    const piHolebaseUrl = process.env.REACT_APP_PIHOLE_BASE || (window as any)._env_.REACT_APP_PIHOLE_BASE;
-    const piHoleApiKey = process.env.REACT_APP_PIHOLE_KEY || (window as any)._env_.REACT_APP_PIHOLE_KEY;
+	const piHolebaseUrl =
+		process.env.REACT_APP_PIHOLE_BASE ||
+		(window as any)._env_.REACT_APP_PIHOLE_BASE;
+	const piHoleApiKey =
+		process.env.REACT_APP_PIHOLE_KEY ||
+		(window as any)._env_.REACT_APP_PIHOLE_KEY;
 
-    const handleTimeSelect = (
-        event: React.FormEvent<HTMLDivElement>,
-        item?: IDropdownOption
-    ): void => {
-        if (item) setSelectedTime(item.key as string);
-    };
+	const handleTimeSelect = (
+		event: React.FormEvent<HTMLDivElement>,
+		item?: IDropdownOption
+	): void => {
+		if (item) setSelectedTime(item.key as string);
+	};
 
-    const fetchStatus = async (): Promise<void> => {
-        let queryString = `${piHolebaseUrl}api.php?status&auth=${piHoleApiKey}`;
-        try {
-            let status = await axios.get(queryString);
-            if (status.data.status === "enabled") {
-                // Compare as a string
-                setPiholeStatus("enabled"); // Set it as a string
-                setTimer(0);
-                setToggleOn(false);
-            } else {
-                setPiholeStatus("disabled"); // Set it as a string
-            }
-        } catch (error) {
-            console.error("Status Fetch Error:", error);
-        }
-    };
+	const fetchStatus = async (): Promise<void> => {
+		let queryString = `${piHolebaseUrl}api.php?status&auth=${piHoleApiKey}`;
+		try {
+			let status = await axios.get(queryString);
+			if (status.data.status === 'enabled') {
+				// Compare as a string
+				setPiholeStatus('enabled'); // Set it as a string
+				setTimer(0);
+				setToggleOn(false);
+			} else {
+				setPiholeStatus('disabled'); // Set it as a string
+			}
+		} catch (error) {
+			console.error('Status Fetch Error:', error);
+		}
+	};
 
-    const toggleSwitch = async (): Promise<void> => {
-        if (selectedTime) {
-            try {
-                await axios.get(
-                    `${piHolebaseUrl}api.php?disable=${selectedTime}&auth=${piHoleApiKey}`
-                );
-                setToggleOn(true);
-                // Start a timer to update timeLeft
-                let timeLeft = parseInt(selectedTime);
-                timer = setInterval(() => {
-                    timeLeft--;
-                    if (timeLeft <= 0) {
-                        clearInterval(timer);
-                        setToggleOn(false);
-                    }
-                    setTimeLeft(prettyPrintTime(timeLeft)); // Update the timeLeft state
-                }, 1000);
-            } catch (error) {
-                console.error("Switch API Error:", error);
-            }
-        }
-    };
+	const toggleSwitch = async (): Promise<void> => {
+		if (selectedTime) {
+			try {
+				await axios.get(
+					`${piHolebaseUrl}api.php?disable=${selectedTime}&auth=${piHoleApiKey}`
+				);
+				setToggleOn(true);
+				// Start a timer to update timeLeft
+				let timeLeft = parseInt(selectedTime);
+				timer = setInterval(() => {
+					timeLeft--;
+					if (timeLeft <= 0) {
+						clearInterval(timer);
+						setToggleOn(false);
+					}
+					setTimeLeft(prettyPrintTime(timeLeft)); // Update the timeLeft state
+				}, 1000);
+			} catch (error) {
+				console.error('Switch API Error:', error);
+			}
+		}
+	};
 
-    function prettyPrintTime(seconds: number): string {
-        const hours = Math.floor(seconds / 3600);
-        const minutes = Math.floor((seconds - hours * 3600) / 60);
-        const secondsLeft = seconds - hours * 3600 - minutes * 60;
-        return `${hours}h ${minutes}m ${secondsLeft}s`;
-    }
+	function prettyPrintTime(seconds: number): string {
+		const hours = Math.floor(seconds / 3600);
+		const minutes = Math.floor((seconds - hours * 3600) / 60);
+		const secondsLeft = seconds - hours * 3600 - minutes * 60;
+		return `${hours}h ${minutes}m ${secondsLeft}s`;
+	}
 
-    const handleThemeToggle = () => {
-        setIsDarkMode(!isDarkMode);
-    };
-    const handleStatusClick = () => {
-        // Use window.location.href to navigate to the URL
-        window.open(piHolebaseUrl);
-    };
-    const handleLogoClick = () => {
-        axios.get(`${piHolebaseUrl}api.php?enable&auth=${piHoleApiKey}`);
-        setIsLogoHovered(false);
-    };
+	const handleThemeToggle = () => {
+		setIsDarkMode(!isDarkMode);
+	};
+	const handleStatusClick = () => {
+		// Use window.location.href to navigate to the URL
+		window.open(piHolebaseUrl);
+	};
+	const handleLogoClick = () => {
+		axios.get(`${piHolebaseUrl}api.php?enable&auth=${piHoleApiKey}`);
+		setIsLogoHovered(false);
+	};
 
-    const handleLogoHover = (event: React.MouseEvent<HTMLImageElement>) => {
-        const { clientX, clientY } = event;
-        setTooltipPosition({ top: clientY + 15, left: clientX + 100 });
-    }
+	const handleLogoHover = (event: React.MouseEvent<HTMLImageElement>) => {
+		const { clientX, clientY } = event;
+		setTooltipPosition({ top: clientY + 15, left: clientX + 100 });
+	};
 
-    // Define the theme based on the isDarkMode state
-    const currentTheme = isDarkMode ? darkTheme : lightTheme;
+	// Define the theme based on the isDarkMode state
+	const currentTheme = isDarkMode ? darkTheme : lightTheme;
 
-    useEffect(() => {
-        fetchStatus();
-        // Set up an interval to fetch status every 10 seconds
-        const intervalId = setInterval(fetchStatus, 3000);
-        return () => clearInterval(intervalId);
-    }, []);
+	useEffect(() => {
+		fetchStatus();
+		// Set up an interval to fetch status every 10 seconds
+		const intervalId = setInterval(fetchStatus, 3000);
+		return () => clearInterval(intervalId);
+	}, []);
 
-    return (
-        <div className="container">
-            <ThemeProvider applyTo="body" theme={ currentTheme }>
-                <div className="flex-container">
-                    <div className="flex-child box1">
-                        { piholeStatus === "enabled" ? (
-                            <span
-                                role="img"
-                                aria-label="Enabled"
-                                onClick={ handleStatusClick }
-                                style={ { color: "green" } }
-                            >
-                                PiHole Enabled ✅
-                            </span>
-                        ) : (
-                            <span
-                                role="img"
-                                about="Disabled"
-                                onClick={ handleStatusClick }
-                                style={ { color: "red" } }
-                            >
-                                PiHole Disabled ❌
-                            </span>
-                        ) }
-                    </div>
-                    <div className="flex-child box2">
-                        <div className="componentAlignRight">
-                            <Toggle
-                                offText="Light Mode"
-                                onClick={ handleThemeToggle }
-                            />
-                        </div>
-                    </div>
-                </div>
-                <img
-                    src={ piholeLogo }
-                    alt="PiHole Logo"
-                    className="logo"
-                    onClick={ handleLogoClick }
-                    onMouseEnter={ () => setIsLogoHovered(true) }
-                    onMouseMove={ (event) => handleLogoHover(event) }
-                    onMouseLeave={ () => setIsLogoHovered(false) }
-                />
-                { isLogoHovered && (
-                    <div className="logoHoverText" style={ { top: tooltipPosition.top, left: tooltipPosition.left } }>Click to start ad blocking again.</div>
-                ) }
-                <p>
-                    Choose for how long you want to disable the ad blocking
-                    service:
-                </p>
-                <Dropdown
-                    options={ timeOptions }
-                    selectedKey={ selectedTime }
-                    onChange={ handleTimeSelect }
-                    placeholder="Select time"
-                    label="Disable Ad Blocking"
-                />
-                <Toggle
-                    className="switch"
-                    checked={ toggleOn }
-                    onChange={ toggleSwitch }
-                />
+	return (
+		<div className="container">
+			<ThemeProvider applyTo="body" theme={currentTheme}>
+				<div className="flex-container">
+					<div className="flex-child box1">
+						{piholeStatus === 'enabled' ? (
+							<span
+								role="img"
+								aria-label="Enabled"
+								onClick={handleStatusClick}
+								style={{ color: 'green' }}
+							>
+								PiHole Enabled ✅
+							</span>
+						) : (
+							<span
+								role="img"
+								about="Disabled"
+								onClick={handleStatusClick}
+								style={{ color: 'red' }}
+							>
+								PiHole Disabled ❌
+							</span>
+						)}
+					</div>
+					<div className="flex-child box2">
+						<div className="componentAlignRight">
+							<Toggle
+								offText="Light Mode"
+								onClick={handleThemeToggle}
+							/>
+						</div>
+					</div>
+				</div>
+				<img
+					src={piholeLogo}
+					alt="PiHole Logo"
+					className="logo"
+					onClick={handleLogoClick}
+					onMouseEnter={() => setIsLogoHovered(true)}
+					onMouseMove={(event) => handleLogoHover(event)}
+					onMouseLeave={() => setIsLogoHovered(false)}
+				/>
+				{isLogoHovered && (
+					<div
+						className="logoHoverText"
+						style={{
+							top: tooltipPosition.top,
+							left: tooltipPosition.left,
+						}}
+					>
+						Click to start ad blocking again.
+					</div>
+				)}
+				<p>
+					Choose for how long you want to disable the ad blocking
+					service:
+				</p>
+				<Dropdown
+					options={timeOptions}
+					selectedKey={selectedTime}
+					onChange={handleTimeSelect}
+					placeholder="Select time"
+					label="Disable Ad Blocking"
+				/>
+				<Toggle
+					className="switch"
+					checked={toggleOn}
+					onChange={toggleSwitch}
+				/>
 
-                { toggleOn && timeLeft !== null && (
-                    <p>Time left: { timeLeft } seconds</p>
-                ) }
-                <Graph />
-            </ThemeProvider>
-        </div>
-    );
+				{toggleOn && timeLeft !== null && (
+					<p>Time left: {timeLeft} seconds</p>
+				)}
+				<Graph />
+			</ThemeProvider>
+		</div>
+	);
 };
 export default PiholeSwitcher;
