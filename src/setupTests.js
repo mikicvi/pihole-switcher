f8a726e1fe482129d91f22fd30c7ecdce59e60f3
mikicvi <88291034+mikicvi@users.noreply.github.com>
2024-04-22 11:14:00 +0100

    feat: GH workflow

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,6 +1,9 @@
 require('dotenv').config();
 
+// mock env variables
+process.env.REACT_APP_PIHOLE_BASE = 'http://localhost';
+process.env.REACT_APP_PIHOLE_KEY = 'asdfmock123key456';
+
 module.exports = {
     setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
-
 };
